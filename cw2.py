6cd7f7a16a0b20998cab3f3c58814707dd93b5e6
s1951693 <s1951693@ed.ac.uk>
2022-12-06 15:35:18 +0000

    a lot of commented

--- cw2.py
+++ cw2.py
@@ -1,31 +1,92 @@
+import collections
+import csv
 
 def retrieved_docs(filename):
     
+    # with open(filename, 'r') as f:
+
+    #     lines = f.readlines()
+    #     retrieved_doc = {}
+    #     sys_retrieved_doc = {}
+
+    #     print(lines[0])
+    #     for line in lines[1:]:
+    #         system_number,query_number,doc_number,rank_of_doc,score = line.split(',')
+    #         score = score[:-1]
+    #         # print(system_number,query_number,doc_number,rank_of_doc,score)
+    #         # break
+    #         # doc_details = (rank_of_doc, score)
+    #         # if query_number in retrieved_doc:
+    #         #     retrieved_doc[query_number].update({doc_number:([rank_of_doc, score])})
+    #         # else:
+    #         #     retrieved_doc.setdefault(query_number, {doc_number:([rank_of_doc, score])})
+
+    #         # if system_number in sys_retrieved_doc:
+    #         #     sys_retrieved_doc[system_number].update(retrieved_doc)
+    #         # else:
+    #         #     sys_retrieved_doc.setdefault(system_number, retrieved_doc)
+            
+    #         if system_number in sys_retrieved_doc:
+    #             if query_number in sys_retrieved_doc[system_number]:
+    #                 sys_retrieved_doc[system_number][query_number].update({doc_number:([rank_of_doc, score])})
+    #             else:
+    #                 sys_retrieved_doc[system_number].setdefault(query_number, {doc_number:([rank_of_doc, score])})
+    #         else:
+    #             sys_retrieved_doc.setdefault(system_number, {query_number, {doc_number:([rank_of_doc, score])}})
+    # results = collections.defaultdict(lambda: collections.defaultdict(list))
+    # with open(filename) as f:
+    #     reader = csv.reader(f, delimiter=',')
+    #     next(reader)
+    #     for row in reader:
+    #         # print(int(row["system_number"]))
+    #         # print(int(row[1]))
+    #         # print(int(row[2]))
+    #         print([row[3:],row[4]])
+    #         results[int(row[0])][int(row[1])][int(row[2])][int(row[3])] = float(row[4])
+    #         # results[row["system_number"]][row["query_number"]][row["doc_number"]][row["rank_of_doc"]] = row["score"]
+    # print(results)
+
+    # def nesteddict():
+    #     return collections.defaultdict(nesteddict)
+
+    # results = nesteddict()
+
+    # f = open(filename)
+    # data = csv.DictReader(f, delimiter=",")
+    # print(data)
+    # for row in data:
+    #     results[row["system_number"]][row["query_number"]][row["doc_number"]][row["rank_of_doc"]] = row["score"]
+
+    # print(results)
+    # return results
+
+    # data = csv.DictReader(f, delimiter=",")
+        # for row in data:
+        #     if row["R"] != "R":
+        #         item = new_data_dict.get(row["UID"], dict())
+        #         item[row["BID"]] = int(row["R"])
+            
+        #         temp_dict = new_data_dict.get(row["FID"], dict())
+        #         if row["UID"] in temp_dict:
+        #             temp_dict[row["UID"]].update(item)
+        #         else:
+        #             temp_dict[row["UID"]] = item
+            
+        #         new_data_dict[row["FID"]] = temp_dict
+
+    # print (results)
+
+    results = collections.defaultdict(lambda: collections.defaultdict(list))
     with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)
+        for row in reader:
+            results[int(row[0])][int(row[1])].append(row[2:])
+        
+    return results
 
-        lines = f.readlines()
-        retrieved_doc = {}
-        sys_retrieved_doc = {}
-
-        print(lines[0])
-        for line in lines[1:]:
-            system_number,query_number,doc_number,rank_of_doc,score = line.split(',')
-            score = score[:-1]
-            # print(system_number,query_number,doc_number,rank_of_doc,score)
-            # break
-            # doc_details = (rank_of_doc, score)
-            if query_number in retrieved_doc:
-                retrieved_doc[query_number].update({doc_number:([rank_of_doc, score])})
-            else:
-                retrieved_doc.setdefault(query_number, {doc_number:([rank_of_doc, score])})
-
-            if system_number in sys_retrieved_doc:
-                sys_retrieved_doc[system_number].update(retrieved_doc)
-            else:
-                sys_retrieved_doc.setdefault(system_number, retrieved_doc)
-
-    # print(sys_retrieved_doc)
-    return sys_retrieved_doc
+def Extract(lst):
+    return list(next(zip(*lst)))
 
 def relevant_docs(filename):
 
@@ -76,24 +137,29 @@ def AP(retrieved_docs, relevant_docs):
 sys_retrieved_doc = retrieved_docs("/Users/arnav/Desktop/Y4/ttds/cw2/system_results.csv")
 relevant_doc = relevant_docs("/Users/arnav/Desktop/Y4/ttds/cw2/qrels.csv")
 
-for _ ,retrieved_doc in sys_retrieved_doc.items():
+for system,retrieved_doc in sys_retrieved_doc.items():
+    print(system, retrieved_doc.keys())
+    print("//////////////////////////////////////////////////////////")
 
-    for query, _ in retrieved_doc.items():
+    for query,doc in retrieved_doc.items():
+        print(doc[:10])
 
-        retrieved_first_10 = retrieved_first_n(retrieved_doc[query], 10)
-        retrieved_first_20 = retrieved_first_n(retrieved_doc[query], 20)
-        retrieved_first_50 = retrieved_first_n(retrieved_doc[query], 50)
+        retrieved_first_10 = doc[:10] # retrieved_first_n(doc, 10)
+        retrieved_first_20 = doc[:20] # retrieved_first_n(doc, 20)
+        retrieved_first_50 = doc[:50] # retrieved_first_n(doc, 50)
+        # print(relevant_doc[query])
+        # print(retrieved_first_10)
 
         relevant_doc_q = [i for i in relevant_doc[query].keys()]
 
-        precision_10 = precision(retrieved_first_10.keys(), relevant_doc_q)
+        precision_10 = precision(Extract(retrieved_first_10), relevant_doc_q)
 
-        recall_50 = recall(retrieved_first_50.keys(), relevant_doc_q)
+        recall_50 = recall(Extract(retrieved_first_50), relevant_doc_q)
 
-        cut_off = retrieved_first_n(retrieved_doc[query], len(relevant_doc_q))
-        r_precision = precision(cut_off.keys(), relevant_doc_q)
+        cut_off = doc[:len(relevant_doc_q)]
+        r_precision = precision(Extract(cut_off), relevant_doc_q)
 
-        ap = AP(retrieved_doc[query], relevant_doc_q)
+        ap = AP(doc, relevant_doc_q)
         print(precision_10)
-
+        break
         
