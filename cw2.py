d6577447bb1d147312bc306ea38fbf92f8376539
s1951693 <s1951693@ed.ac.uk>
2022-12-07 05:59:23 +0000

    MI and chi done!!

--- cw2.py
+++ cw2.py
@@ -1,6 +1,8 @@
 import collections
 import csv
 import numpy as np
+import re
+from nltk.stem import PorterStemmer
 
 def retrieved_docs(filename):
 
@@ -69,11 +71,10 @@ def nDCG(retrieved_docs, relevant_docs):
     nDCG = DCG/iDCG
 
     return round(nDCG, 3)
-        
-sys_retrieved_doc = retrieved_docs("/Users/arnav/Desktop/Y4/ttds/cw2/system_results.csv")
-relevant_doc = relevant_docs("/Users/arnav/Desktop/Y4/ttds/cw2/qrels.csv")
 
 with open('ir_eval.csv', 'w') as result:
+    sys_retrieved_doc = retrieved_docs("/Users/arnav/Desktop/Y4/ttds/cw2/system_results.csv")
+    relevant_doc = relevant_docs("/Users/arnav/Desktop/Y4/ttds/cw2/qrels.csv")
     writer = csv.writer(result, delimiter=",")
     writer.writerow(['system_number','query_number','P@10','R@50','r-precision','AP','nDCG@10','nDCG@20'])
 
@@ -102,3 +103,145 @@ with open('ir_eval.csv', 'w') as result:
         means.append(np.mean(for_mean, axis=0))
         writer.writerow([system,"mean",round(means[system-1][0],3),round(means[system-1][1],3),round(means[system-1][2],3),
         round(means[system-1][3],3),round(means[system-1][4],3),round(means[system-1][5],3)])
+
+
+
+
+
+
+
+
+
+
+
+
+
+# preprosses the input text
+def preprocessing(text):
+    from string import punctuation
+    # Eliminate duplicate whitespaces using wildcards
+    text = re.sub('\s+', ' ', text)
+    # checks for all the alphanumeric characters, spaces and punctuations and keeps them in the corpus
+    text = ''.join(t for t in text if (t.isalnum() or t.isspace() or t in punctuation))
+    # replaces all the punctiations with spaces
+    regex = re.compile('[%s]' % re.escape(punctuation))
+    text = regex.sub(' ', text)
+    # splits the entire text at blank spaces
+    text = text.split()
+
+    # converts the entire vocabulary into lower case
+    words = []
+    for word in text:
+        words.append(word.lower())
+    
+    # Stop words removal
+    stopwords = open("/Users/arnav/Desktop/Y4/ttds/cw1/englishST.txt").read()
+    processed_text = [word for word in words if word not in stopwords]
+    # print(processed_text[:100])
+
+    # Normalization using Porter stemmer
+    porter = PorterStemmer()
+    stem_words = [porter.stem(word) for word in processed_text]
+    stem_words_without_stopping = [porter.stem(word) for word in words]
+    # print(stem_words[:80])
+
+    # words after stopping and stemming
+    # return stem_words
+    # comment this return statement out to return without stopping
+    # print(stem_words)
+    return stem_words
+
+
+def tsv_reader(filename):
+
+    classes = collections.defaultdict(lambda: collections.defaultdict(list))
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        # lines = f.readlines()
+        # print(next(reader))
+        # print(a)
+        text = []
+        for row in reader:
+            # claus, text = line.split('\t')
+            if row[0] in classes:
+                text = preprocessing(row[1]) 
+                classes[row[0]] += text
+            else:
+                classes.setdefault(row[0], text)
+    # print(classes)
+    return classes
+
+def dict_count(classes):
+
+    # print(classes.values())
+    total = []
+    count_ele_class = {}
+    for claus, text in classes.items():
+        total += text
+
+        count_ele_class[claus] = dict(collections.Counter(text))
+        
+    tots = dict(collections.Counter(total))
+    total = sum(list(tots.values()))
+    
+    return count_ele_class, total
+
+def MI_calc(N_11, N_10, N_00, N_01, N_1, N_0, N1_, N0_, N):
+    t1 = 0 if N_11 == 0 else (N_11/N * np.log2((N * N_11)/(N1_ * N_1)))
+    t2 = 0 if N_10 == 0 else (N_10/N * np.log2((N * N_10)/(N1_ * N_0)))
+    t3 = 0 if N_01 == 0 else (N_01/N * np.log2((N * N_01)/(N0_ * N_1)))
+    t4 = 0 if N_00 == 0 else (N_00/N * np.log2((N*N_00)/(N0_ * N_0)))
+
+    return t1+t2+t3+t4
+
+def chi_sq_calc(N_11, N_10, N_00, N_01, N):
+    return ((N_10 + N_11 + N_01 + N_00) * np.power((N_11 * N_00 - N_10 * N_01),2)) / ((N_01 + N_11) * (N_10 + N_11) * (N_10 + N_00) * (N_01 + N_00))
+
+def mi_chi(class_word_count, total_words):
+
+    x = []
+    classes_MI = {}
+    classes_chi = {}
+    for classes, words in class_word_count.items():
+
+        total_in_class = sum(list(words.values()))
+
+        MI = collections.defaultdict(dict)
+        chi_sq = collections.defaultdict(dict)
+        print(classes)
+        
+        for word, count in words.items():
+
+            for claus, words in class_word_count.items():
+                if claus != classes:
+                    if word in words.keys():
+                        x.append(words[word])
+            
+            N_10 = sum(x)
+            N_11 = count
+            N_01 = total_in_class - N_11
+            N_00 = total_words - N_11 - N_10 - N_01
+            N = total_words
+
+            N1_ = N_11 + N_10
+            N_1 = N_01 + N_11
+            N0_ = N_01 + N_00
+            N_0 = N_00 + N_10
+
+            # MI[classes][word] = (N_11/N * np.log2((N * N_11)/(N1_ * N_1))) + (N_01/N * np.log2((N * N_01)/(N0_ * N_1))) + (N_10/N * np.log2((N * N_10)/(N1_ * N_0))) + (N_00/N * np.log2((N*N_00)/(N0_ * N_0)))
+            MI[classes][word] = MI_calc(N_11, N_10, N_00, N_01, N_1, N_0, N1_, N0_, N)
+            # chi_sq[classes][word] = ((N_10 + N_11 + N_01 + N_00) * (N_11*N_00 - N_10*N_01)^2)/((N_01 + N_11)*(N_10 + N_11)*(N_10 + N_00)*(N_01 + N_00))
+            chi_sq[classes][word] = chi_sq_calc(N_11, N_10, N_00, N_01, N)
+        
+        classes_MI.update(dict(MI))
+        classes_chi.update(dict(chi_sq))
+
+    return (classes_MI, classes_chi)
+
+
+classes = tsv_reader("/Users/arnav/Desktop/Y4/ttds/cw2/test.tsv")
+# print(classes.values())
+class_word_count, total_words = dict_count(classes)
+
+MI, chi = mi_chi(class_word_count, total_words)
+print(chi)
